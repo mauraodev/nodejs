b1601d13e0c15cfd89254f8863e354b3e24b46db
Mauro Nunes <mauraoemau@hotmail.com>
2018-04-27 14:24:10 -0300

    Estudo de callback

---
+++ callback_heaven.js
@@ -0,0 +1,26 @@
+var fs = require('fs');
+
+var lerDiretorio = function() {
+	fs.readdir(__dirname, function(erro, diretorio) {
+		if (erro) {
+			throw erro;
+		}
+
+		diretorio.forEach(function(arquivo) {
+			ler(arquivo);
+		});
+	});
+};
+
+var ler = function(arquivo) {
+	var path = './' + arquivo;
+	fs.stat(path, function(erro, stat) {
+		if (erro) return erro;
+
+		if (stat.isFile()) {
+			console.log('%s %d bytes', arquivo, stat.size);
+		}
+	});
+};
+
+lerDiretorio();
